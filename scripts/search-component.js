0a45e28989387be44e266cd6b653a8293f07f1d5
jiaxie <jiaxie@thoughtworks.com>
2015-08-17 20:05:56 +0800

    [xj] make moco works for location

--- scripts/search-component.js
+++ scripts/search-component.js
@@ -8,6 +8,7 @@ function SearchComponent(searchInput, searchButton) {
 SearchComponent.prototype.search = function () {
     $.ajax({
         url: 'http://locations-backend.herokuapp.com/locations?location=' + $(this.searchInput).val(),
+        //url: '/locations?location=' + $(this.searchInput).val(),
         dataType: 'json',
         success: function(data){
             $(document).trigger('search', [data]);
@@ -17,58 +18,6 @@ SearchComponent.prototype.search = function () {
         }
     })
 };
-
-//-----------------------------------------
-//(function($){
-//    //var sss = function(){
-//    //
-//    //};
-//    function SearchComponent(searchInput, searchButton) {
-//        this.searchInput = searchInput;
-//        this.searchButton = $(searchButton);
-//        this.searchButton.on('click', _.bind(this.search, this));
-//
-//    };
-//    SearchComponent.prototype.search = function () {
-//        var callBack = function(data) {
-//            $(document).trigger('search', [data]);
-//        }
-//        $.ajax({
-//            url: 'http://locations-backend.herokuapp.com/locations?location=' + $(this.searchInput).val(),
-//            dataType: 'json',
-//            success: callBack,
-//            error: function (error) {
-//                console.log(error);
-//            }
-//
-//        })
-//    };
-//
-//    window.SearchComponent = SearchComponent;
-//})(jQuery);
-
-////------------------------------
-//var searchComponent = (function(searchInput, searchButton){
-//    var searchInput = searchInput;
-//    var searchButton = searchButton;
-//    var callBack = function(data) {
-//        $(document).trigger('search', [data]);
-//    };
-//    var search = function(){
-//        $.ajax({
-//            url: 'http://locations-backend.herokuapp.com/locations?location=' + $(this.searchInput).val(),
-//            dataType: 'json',
-//            success: callBack,
-//            error: function (error) {
-//                console.log(error);
-//            }
-//        })
-//    };
-//
-//    return {
-//        search: search
-//    };
-//})();
 
 
 
--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 9999
+java -jar moco-runner-0.10.1-standalone.jar http -p 8080 -c server.json
